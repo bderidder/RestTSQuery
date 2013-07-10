d22d34d9a79cd8d554a2d2884c3dd88f97c4875c
Bavo De Ridder <bavo@bavoderidder.com>
2013-07-10 13:29:50 +0200

    Initial revision

---
+++ src/main/java/org/coderspotting/ts/query/rest/HashMapUtils.java
@@ -0,0 +1,40 @@
+package org.coderspotting.ts.query.rest;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+
+public class HashMapUtils
+{
+    public static void outputHashMap(HashMap<String, String> hashMap)
+    {
+        if (hashMap == null)
+        {
+            return;
+        }
+
+        Collection<String> cValue = hashMap.values();
+        Collection<String> cKey = hashMap.keySet();
+        Iterator<String> itrValue = cValue.iterator();
+        Iterator<String> itrKey = cKey.iterator();
+
+        while (itrValue.hasNext() && itrKey.hasNext())
+        {
+            System.out.println(itrKey.next() + ": " + itrValue.next());
+        }
+    }
+
+    public static void outputHashMap(List<HashMap<String, String>> hashMapList)
+    {
+        if (hashMapList == null)
+        {
+            return;
+        }
+
+        for(HashMap<String, String> hashMap : hashMapList)
+        {
+            outputHashMap(hashMap);
+        }
+    }
+}
