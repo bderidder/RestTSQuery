bd609fe41d31ddd389201e8122703c3514eb85ef
Bavo De Ridder <bavo@bavoderidder.com>
2013-07-10 13:27:53 +0200

    Initial revision

---
+++ src/main/java/org/coderspotting/ts/query/rest/CouldNotExecuteCommandException.java
@@ -0,0 +1,9 @@
+package org.coderspotting.ts.query.rest;
+
+public class CouldNotExecuteCommandException extends Exception
+{
+    public CouldNotExecuteCommandException(String message)
+    {
+        super(message);
+    }
+}
