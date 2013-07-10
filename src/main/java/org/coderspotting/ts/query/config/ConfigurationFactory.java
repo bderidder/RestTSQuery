380337cc2638b1d7c8166ca16764786b19edbe8b
Bavo De Ridder <bavo@bavoderidder.com>
2013-07-10 09:30:57 +0200

    Added Javadoc. Made it into a pure static Factory class

--- src/main/java/org/coderspotting/ts/query/config/ConfigurationFactory.java
+++ src/main/java/org/coderspotting/ts/query/config/ConfigurationFactory.java
@@ -2,25 +2,27 @@ package org.coderspotting.ts.query.config;
 
 import org.coderspotting.ts.query.config.rt.PropertiesConfiguration;
 
+/**
+ * A factory class to fetch an instance of a Configuration
+ *
+ * @author Bavo De Ridder <http://www.coderspotting.org/>
+ */
 public class ConfigurationFactory
 {
     private static Configuration configuration = null;
-    
-    public ConfigurationFactory()
+
+    /**
+     * Fetch an instance of a Configuration. Subsequent calls to this method will always return the same instance.
+     *
+     * @return singleton instance of a Configuration
+     */
+    public static Configuration getConfiguration()
     {
         if (configuration == null)
         {
-            loadConfiguration();
+            configuration = new PropertiesConfiguration();
         }
-    }
-    
-    public final Configuration getConfiguration()
-    {
+
         return configuration;
-    }
-    
-    private void loadConfiguration()
-    {
-        configuration = new PropertiesConfiguration();
     }
 }
